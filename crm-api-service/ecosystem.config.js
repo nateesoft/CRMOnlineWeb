20da497c73c565dc759acc61d27c09fe733fc79f
nateelive <natee.live@gmail.com>
2022-07-11 20:11:43 +0700

    update config crm service

--- crm-api-service/ecosystem.config.js
+++ crm-api-service/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: 'crm-api-service',
     script: 'app.js',
-    instances: 4,
-    autorestart: true,
+    instances: 1,
+    autorestart: false,
     watch: false,
     max_memory_restart: '2G',
   },]
